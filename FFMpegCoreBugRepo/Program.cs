413b27e1ec91677cb49de62a69db079c1fe90ebf
Renan Pinheiro <renancamerapinheiro@gmail.com>
2020-12-06 19:05:14 -0300

    update to analyse bug

--- FFMpegCoreBugRepo/Program.cs
+++ FFMpegCoreBugRepo/Program.cs
@@ -5,92 +5,16 @@ using FFMpegCore;
 using FFMpegCore.Enums;
 using FFMpegCore.Pipes;
 
-const string FILE_NAME = "Marshmello & Anne-Marie - FRIENDS (Lyric Video) _OFFICIAL FRIENDZONE ANTHEM";
+const string FILE_NAME = "Data/Friends";
 const string MP3_PATH = FILE_NAME + ".mp3";
-const string OPUS_PATH = FILE_NAME + ".opus";
-const string RAW_PATH = FILE_NAME + ".raw";
+// const string OPUS_PATH = FILE_NAME + ".opus";
+// const string RAW_PATH = FILE_NAME + ".raw";
 
 
-using var ms1 = await PipeBroken1Async();
-//using var ms2 = await PipeBroken2Async();
-//using var ms3 = await PipeBroken3Async();
-//using var ms4 = await WorkingPipeAsync();
+using var sourceStream = File.Open(MP3_PATH, FileMode.Open);
 
-static async Task<Stream> PipeBroken1Async()
-{
-    using var file = File.Open(MP3_PATH, FileMode.Open);
+var mediaInfo1 = await FFProbe.AnalyseAsync(sourceStream);
+var mediaInfo2 = await FFProbe.AnalyseAsync(MP3_PATH);
 
-    var memoryStream = new MemoryStream();
-
-    // Try convert to opus
-    await FFMpegArguments
-            .FromPipeInput(new StreamPipeSource(file))
-            .OutputToPipe(new StreamPipeSink(memoryStream), options =>
-            {
-                options.WithAudioSamplingRate(48000);
-                options.WithAudioCodec("libopus");
-                options.WithCustomArgument("-ac 2");
-            })
-            .ProcessAsynchronously();
-
-    return memoryStream;
-}
-
-static async Task<Stream> PipeBroken2Async()
-{
-    using var file = File.Open(OPUS_PATH, FileMode.Open);
-
-    var memoryStream = new MemoryStream();
-
-    // Try convert to mp3
-    await FFMpegArguments
-            .FromPipeInput(new StreamPipeSource(file))
-            .OutputToPipe(new StreamPipeSink(memoryStream), options =>
-            {
-                options.WithAudioSamplingRate(48000);
-                options.WithAudioCodec(AudioCodec.LibMp3Lame);
-                options.WithCustomArgument("-ac 2");
-            })
-            .ProcessAsynchronously();
-
-    return memoryStream;
-}
-
-static async Task<Stream> PipeBroken3Async()
-{
-    using var file = File.Open(RAW_PATH, FileMode.Open);
-
-    var memoryStream = new MemoryStream();
-
-    // Try convert to opus
-    await FFMpegArguments
-            .FromPipeInput(new StreamPipeSource(file))
-            .OutputToPipe(new StreamPipeSink(memoryStream), options =>
-            {
-                options.WithAudioSamplingRate(48000);
-                options.WithAudioCodec("libopus");
-                options.WithCustomArgument("-ac 2");
-            })
-            .ProcessAsynchronously();
-
-    return memoryStream;
-}
-
-static async Task<Stream> WorkingPipeAsync()
-{
-    using var file = File.Open(MP3_PATH, FileMode.Open);
-
-    var memoryStream = new MemoryStream();
-
-    // Try convert to opus
-    await FFMpegArguments
-            .FromPipeInput(new StreamPipeSource(file))
-            .OutputToPipe(new StreamPipeSink(memoryStream), options =>
-            {
-                options.WithAudioSamplingRate(48000);
-                options.WithCustomArgument("-ac 2 -f s16le");
-            })
-            .ProcessAsynchronously();
-
-    return memoryStream;
-}
+var duration1 = mediaInfo1.Duration;
+var duration2 = mediaInfo2.Duration;
